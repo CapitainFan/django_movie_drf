2e337b24a5bf9793aba87d0610cd9ca03512c432
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2024-02-19 21:51:54 +0300

    added createrevewserializer

--- movies/views.py
+++ movies/views.py
@@ -1,19 +1,30 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from .models import Movie
-from .serializers import MovieListSerializer, MovieDetailSerializer
+from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer
 
 
 class MovieListView(APIView):
-    def get(self, requests):
+    """Вывод списка фильмов"""
+    def get(self, request):
         movies = Movie.objects.filter(draft=False)
         serializer = MovieListSerializer(movies, many=True)
         return Response(serializer.data)
 
 
-
 class MovieDetailView(APIView):
-    def get(self, requests, pk):
+    """Вывод фильма"""
+    def get(self, request, pk):
         movie = Movie.objects.get(id=pk, draft=False)
         serializer = MovieDetailSerializer(movie)
         return Response(serializer.data)
+
+
+class ReviewCreateView(APIView):
+    """Добавление отзыва к фильму"""
+    def post(self, request):
+        review = ReviewCreateSerializer(data=request.data)
+        if review.is_valid():
+            review.save()
+        return Response(status=201)
